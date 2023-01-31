ee87b712aed64a827814f3c9a59f13b912cc13c7
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2023-01-31 13:24:24 +0530

    sm6150-common: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -164,6 +164,10 @@ PRODUCT_PACKAGES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
+    
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
