136be5292ff04fccafbdc7b8dbe56a097ceb08e8
helliscloser <mrumais@gmail.com>
2023-06-27 04:38:57 +0000

    Remove some packages from build
    Co-authored-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

---
+++ Remove/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackagesDavinci
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Seedvault \
+    Jelly \
+    Calendar
+    
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
--- sm6150.mk
+++ sm6150.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
+    
+#Remove Packages from Build
+PRODUCT_PACKAGES += \
+    RemovePackagesDavinci  
 
 # Sensors
 PRODUCT_COPY_FILES += \
