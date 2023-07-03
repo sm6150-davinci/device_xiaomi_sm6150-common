ebee18a82e44aaf69d8f73f7a1edd0a340f03d5a
SebaUbuntu <barezzisebastiano@gmail.com>
2023-07-03 19:40:50 +0300

    sm6150-common: Switch to libinit from 8150-common lineage

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,10 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_sm6150
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_sm6150
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
