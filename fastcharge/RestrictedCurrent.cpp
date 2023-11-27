elpaablo/device_xiaomi_grus

Extend Fastcharge implementation [3/3]
* Add implementation for setting max current allowed when not fast charging

fastcharge/RestrictedCurrent.cpp
@@ -16,14 +16,15 @@
 
 #define LOG_TAG "fastcharge@1.0-service.xiaomi"
 
-#define FASTCHARGE_DEFAULT_SETTING true
-#define FASTCHARGE_PATH "/sys/class/qcom-battery/restricted_charging"
+#define RESTRICTED_CURRENT_PATH "/sys/class/qcom-battery/restricted_current"
 
-#include "FastCharge.h"
+#define MAX_SUPPORTED_CURRENT 3000 /*mA*/
+
+#include "RestrictedCurrent.h"
 #include <android-base/logging.h>
-
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
 
 namespace vendor {
 namespace lineage {
@@ -72,17 +73,37 @@ template <typename T> static T get(const std::string &path, const T &def) {
   }
 }
 
-FastCharge::FastCharge() {}
+RestrictedCurrent::RestrictedCurrent() {}
 
-Return<bool> FastCharge::isEnabled() { return get(FASTCHARGE_PATH, 0) < 1; }
+/*
+ * Get the restricted current value from the corresponding sysnode
+ */
+Return<int32_t> RestrictedCurrent::getRestrictedCurrent() {
+  int current_uA = get(RESTRICTED_CURRENT_PATH, 0); /* current is stored in microampere */
+  int current_mA = current_uA / 1000; /* convert to miliampere */
+  return current_mA; /* always return mA */
+}
 
-Return<bool> FastCharge::setEnabled(bool enable) {
+/*
+ * Set the maximum allowed current (restricted current), when fast charging is disabled
+ */
+Return<bool> RestrictedCurrent::setRestrictedCurrent(int32_t current_mA) {
   bool success = false;
-  set(FASTCHARGE_PATH, enable ? 0 : 1);
-  if (enable == isEnabled()){
-    success = true;
+  if (current_mA > 0 && current_mA <= MAX_SUPPORTED_CURRENT) { /* validate */
+    int current_uA = current_mA * 1000; /* convert to microampere */
+    set(RESTRICTED_CURRENT_PATH, current_uA); /* store */
+    if (current_mA == getRestrictedCurrent()) { /* check */
+      success = true;
+    }
   }
   return success;
+}
+
+/*
+ * Get the maximum supported current
+ */
+Return<int32_t> RestrictedCurrent::getMaxSupportedCurrent() {
+  return MAX_SUPPORTED_CURRENT; /* always return mA */
 }
 
 } // namespace implementation

sm6150-davinci/device_xiaomi_sm6150-common

fastcharge/RestrictedCurrent.cpp
@@ -16,14 +16,15 @@
 
 #define LOG_TAG "fastcharge@1.0-service.xiaomi"
 
-#define FASTCHARGE_DEFAULT_SETTING true
-#define FASTCHARGE_PATH "/sys/class/qcom-battery/restrict_chg"
+#define RESTRICTED_CURRENT_PATH "/sys/class/qcom-battery/restrict_cur"
 
-#include "FastCharge.h"
+#define MAX_SUPPORTED_CURRENT 3000 /*mA*/
+
+#include "RestrictedCurrent.h"
 #include <android-base/logging.h>
-
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
 
 namespace vendor {
 namespace lineage {
@@ -72,17 +73,37 @@ template <typename T> static T get(const std::string &path, const T &def) {
   }
 }
 
-FastCharge::FastCharge() {}
+RestrictedCurrent::RestrictedCurrent() {}
 
-Return<bool> FastCharge::isEnabled() { return get(FASTCHARGE_PATH, 0) < 1; }
+/*
+ * Get the restricted current value from the corresponding sysnode
+ */
+Return<int32_t> RestrictedCurrent::getRestrictedCurrent() {
+  int current_uA = get(RESTRICTED_CURRENT_PATH, 0); /* current is stored in microampere */
+  int current_mA = current_uA / 1000; /* convert to miliampere */
+  return current_mA; /* always return mA */
+}
 
-Return<bool> FastCharge::setEnabled(bool enable) {
+/*
+ * Set the maximum allowed current (restricted current), when fast charging is disabled
+ */
+Return<bool> RestrictedCurrent::setRestrictedCurrent(int32_t current_mA) {
   bool success = false;
-  set(FASTCHARGE_PATH, enable ? 0 : 1);
-  if (enable == isEnabled()){
-    success = true;
+  if (current_mA > 0 && current_mA <= MAX_SUPPORTED_CURRENT) { /* validate */
+    int current_uA = current_mA * 1000; /* convert to microampere */
+    set(RESTRICTED_CURRENT_PATH, current_uA); /* store */
+    if (current_mA == getRestrictedCurrent()) { /* check */
+      success = true;
+    }
   }
   return success;
+}
+
+/*
+ * Get the maximum supported current
+ */
+Return<int32_t> RestrictedCurrent::getMaxSupportedCurrent() {
+  return MAX_SUPPORTED_CURRENT; /* always return mA */
 }
 
 } // namespace implementation