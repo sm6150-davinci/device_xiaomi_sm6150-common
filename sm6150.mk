46cced0deb81eb21fa46d5f2dbdc928d9cdf5380
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-01-21 10:46:40 +0800

    sm6150-common: Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    - Do this only in user/debug builds (except eng).
    - Add wifi and cnss too while we're at it, they can get noisy at times.
    
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -225,6 +225,27 @@ $(call soong_config_set,lineage_health,charging_control_supports_bypass,true)
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
